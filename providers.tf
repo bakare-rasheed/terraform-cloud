53abbc63f7590eec396fddb416eb5f04667d4c2b
bakare-rasheed <bakarerasheed.a38@gmail.com>
2023-04-09 17:52:47 +0000

    new

--- providers.tf
+++ providers.tf
@@ -9,7 +9,14 @@ terraform {
 
 provider "aws" {
   region = var.region
-  access_key = {}
-  secret_key = {}
+  #  access_key = var.access_key
+  # secret_key = var.secret_key
+}
 
+variable "access_key" {
+  default = terraform.workspace == "production" ? "" : var.access_key
+}
+
+variable "secret_key" {
+  default = terraform.workspace == "production" ? "" : var.secret_key
 }
